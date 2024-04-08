37fc42b2270bb3e17ee77a36007bf4dd53f55085
MugnusD <122955763+MugnusD@users.noreply.github.com>
2024-04-08 11:21:45 +0800

    remove

--- test/unit/utils/wiki_text_processing/story/story.test.ts
+++
@@ -1,8 +0,0 @@
-
-
-describe('story test', () => {
-    it('fetch story', async () => {
-        await fetch('https://sirius.3-3.dev/scene/140911.json')
-            .then(response => response.json())
-            })
-    })
